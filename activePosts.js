c5a65c73eabe159fac2fa9a81e30abde112212c3
danielbs1406 <danielbs1406@hotmail.com>
2016-06-06 19:04:10 -0300

    index page only

--- activePosts.js
+++ activePosts.js
@@ -1,6 +1,6 @@
 (function ($) {
     var amount = '5';
-    if (location.href.indexOf('/site') === -1 && location.href.indexOf('/search/?c=5') === -1) {
+    if (location.href.indexOf('/index') !== -1 && location.href.indexOf('/search/?c=5') === -1) {
         $('div.category').last().before('<br/><div id="sidebar"><div id="portal"><div id="latest" class="portal_box"><h2>Últimas Postagens</h2></div><div></div>');
         $.get(main_url + 'search/?c=5&force_ads', function (data) {
 			$(data).find('.forums').appendTo('#latest');
